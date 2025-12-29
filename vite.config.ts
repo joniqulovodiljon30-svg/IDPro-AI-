076940978b8562683cc116dc8e456f3c4f3cd27d
Odilxon <joniqulovodiljon30@gmail.com>
2025-12-30 00:05:52 +0500

    Update API keys in vite.config.ts
    Signed-off-by: Odilxon <joniqulovodiljon30@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.API_KEY': JSON.stringify(env.AIzaSyBHG-NxqydJL-_Um5LLB62iiI5OEZ5H91Q),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.AIzaSyBHG-NxqydJL-_Um5LLB62iiI5OEZ5H91Q)
       },
       resolve: {
         alias: {
